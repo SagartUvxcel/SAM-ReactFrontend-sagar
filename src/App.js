0ee3f36eda909a7eaf42029f71b17a2411e42fc3
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-11-07 11:21:33 +0530

    Edited on 7-11-2022

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/2.HomePage/Home";
@@ -6,11 +5,9 @@ import ViewPropertyDetails from "./components/2.HomePage/ViewPropertyDetails";
 import LoginMainPage from "./components/6.Login/LoginMainPage";
 import ResetPassword from "./components/7.Registration/ResetPassword";
 import Registration from "./components/7.Registration/RegistrationMainPage";
-import VerifyToken from "./components/7.Registration/VerifyToken";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  const [token, setToken] = useState("");
   return (
     <BrowserRouter>
       <ScrollToTop>
@@ -18,14 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Home />} />
           <Route path="/property" element={<ViewPropertyDetails />} />
-          <Route
-            path="/register/*"
-            element={<Registration setToken={setToken} />}
-          />
-          <Route
-            path="/register/verify"
-            element={<VerifyToken token={token} />}
-          />
+          <Route path="/register/*" element={<Registration />} />
           <Route path="/login" element={<LoginMainPage />} />
           <Route path="/register/reset-password" element={<ResetPassword />} />
         </Routes>
