bd4f7ab5038f8411aeba42d2b97e48d6fdf10ce1
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-12-15 14:42:32 +0530

    Edited on 15-12-2022

--- src/Admin/ViewEditProperty.js
+++ src/Admin/ViewEditProperty.js
@@ -1,18 +1,34 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Layout from "../components/1.CommonLayout/Layout";
 import propertyData from "./data.json";
 
 const ViewEditProperty = () => {
   const { id } = useParams();
+  const [property, setProperty] = useState([]);
+
+  const setCurrentProperty = () => {
+    for (let i of propertyData) {
+      if (i.id === id) {
+        setProperty(i);
+      }
+    }
+  };
+
   useEffect(() => {
-    console.log(id);
-  }, []);
+    setCurrentProperty();
+  });
 
   return (
-    <section className="mt-5 admin-edit-property wrapper">
-      <div className="container-fluid">View & Edit</div>
-    </section>
+    <Layout>
+      <section className="mt-5 admin-edit-property wrapper min-100vh">
+        <div className="container-fluid">
+          View & Edit - {property.city_name}
+        </div>
+      </section>
+    </Layout>
   );
 };
 
