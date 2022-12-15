f73bb06d91c7c9d72752d88b27f9cdcdf3f17ba1
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-12-15 16:57:10 +0530

    Edited on 15-12-2022

--- src/Admin/AdminSideBar.js
+++ src/Admin/AdminSideBar.js
@@ -3,19 +3,22 @@ import { Link, NavLink } from "react-router-dom";
 
 const AdminSideBar = () => {
   return (
-    <div className="col-xl-2 col-md-3 bg-info">
-      <div className="admin-sidebar py-3">
-        <Link to="/" className="offcanvas-header nav-link">
-          <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+    <div className="col-xl-2 col-md-3 admin-sidebar">
+      <div className="py-3">
+        <Link
+          to="/admin/home"
+          className="offcanvas-header text-decoration-none text-white"
+        >
+          <h4 className="offcanvas-title" id="offcanvasExampleLabel">
             Admin Panel
-          </h5>
+          </h4>
         </Link>
         <div className="offcanvas-body mt-5">
           <ul className="navbar-nav">
             <li className="nav-item">
               <NavLink className="nav-link" to="/admin/view-properties">
                 <span className="mx-2">
-                  <i className="bi bi-buildings-fill"></i>
+                  <i className="bi bi-buildings-fill text-dark"></i>
                 </span>
                 All Properties
               </NavLink>
