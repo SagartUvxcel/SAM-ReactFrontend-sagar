93a4b5c47f20bb08d3155747ceecddc11ad2169a
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-12-16 15:19:26 +0530

    Edited on 16-12-2022

--- src/Admin/AdminHomePage.js
+++ src/Admin/AdminHomePage.js
@@ -7,11 +7,55 @@ const AdminHomePage = () => {
       <div className="row vh-100">
         <AdminSideBar />
         <div className="col-xl-10 col-md-9">
-          <div className="mt-5 wrapper">
-            <div className="container">
-              <h1 className="text-center py-5">
-                Home Page or Dashboard of Admin
-              </h1>
+          <div className="container-fluid wrapper admin-home-wrapper">
+            <div className="row">
+              <div className="col-xl-3">
+                <div className="card justify-content-center d-flex flex-row align-items-center py-3">
+                  <span className="me-5">
+                    <i className="bi bi-buildings-fill text-primary fs-1"></i>
+                  </span>
+                  <div>
+                    <span className="admin-dashboard-count">180</span>
+                    <h5 className="text-primary">Properties</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xl-3">
+                <div className="card justify-content-center d-flex flex-row align-items-center py-3">
+                  <span className="me-5">
+                    <i className="bi bi-person-fill text-primary fs-1"></i>
+                  </span>
+                  <div>
+                    <span className="admin-dashboard-count">110</span>
+                    <h5 className="text-primary">Users</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xl-3">
+                <div className="card justify-content-center d-flex flex-row align-items-center py-3">
+                  <span className="me-5">
+                    <i className="bi bi-buildings-fill text-primary fs-1"></i>
+                  </span>
+                  <div>
+                    <span className="admin-dashboard-count">180</span>
+                    <h5 className="text-primary">Properties</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xl-3">
+                <div className="card justify-content-center d-flex flex-row align-items-center py-3">
+                  <span className="me-5">
+                    <i className="bi bi-buildings-fill text-primary fs-1"></i>
+                  </span>
+                  <div>
+                    <span className="admin-dashboard-count">180</span>
+                    <h5 className="text-primary">Properties</h5>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
