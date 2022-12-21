043b46c566cf18391726dd0e39ab084f62480c2f
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-12-21 10:57:28 +0530

    Edited on 21-12-2022

--- src/Admin/ManageUsers.js
+++ src/Admin/ManageUsers.js
@@ -1,13 +1,42 @@
 import React from "react";
 import AdminSideBar from "./AdminSideBar";
+import users from "./users.json";
 
 const ManageUsers = () => {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid admin-users-wrapper">
       <div className="row vh-100">
         <AdminSideBar />
         <div className="col-xl-10 col-md-9 wrapper">
-          <h1 className="text-center">Manage Users</h1>
+          <h1 className="text-center">Users</h1>
+          <div className="table-wrapper">
+            <table className="table table-bordered table-dark">
+              <thead>
+                <tr>
+                  <th>Sr. No.</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Company Name</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user, Index) => {
+                  return (
+                    <tr key={Index}>
+                      <td>{Index + 1}</td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.phone}</td>
+                      <td>{user.company.name}</td>
+                      <td>Edit</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
