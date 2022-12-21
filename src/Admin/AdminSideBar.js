3152a35963548043758cc1198051d5821e8914a1
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-12-21 13:11:09 +0530

    Edited on 21-12-2022

--- src/Admin/AdminSideBar.js
+++ src/Admin/AdminSideBar.js
@@ -13,25 +13,14 @@ const AdminSideBar = () => {
     <div className="col-xl-2 col-md-3 admin-sidebar">
       <div className="py-3">
         <span className="offcanvas-header text-white">
-          <h4 className="offcanvas-title" id="offcanvasExampleLabel">
-            Admin Panel
+          <h4 className="offcanvas-title ps-4" id="offcanvasExampleLabel">
+            Admin
           </h4>
         </span>
 
         <div className="offcanvas-body mt-4">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink
-                className="text-white nav-link text-decoration-none"
-                to="/"
-              >
-                <span className="mx-2">
-                  <i className="bi bi-arrow-bar-left text-secondary"></i>
-                </span>
-                Sam Tool
-              </NavLink>
-            </li>
-            <hr />
+            <hr className="text-white" />
             <li className="nav-item">
               <NavLink className="nav-link admin-home-link" to="/admin">
                 <span className="mx-2">
@@ -56,7 +45,7 @@ const AdminSideBar = () => {
                 Users
               </NavLink>
             </li>
-            <hr />
+            <hr className="text-white" />
             <li className="nav-item">
               <NavLink className="nav-link" to="/admin/upload-properties">
                 <span className="mx-2">
